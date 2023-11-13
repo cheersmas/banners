55f1d2d1c3bd11e427469a3afb2c8772bf004200
cheersmas <shradhankr0028@gmail.com>
2023-11-14 00:04:56 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { StripeCards } from './StripeCards'
+
+function App() {
+
+  return (
+    <>
+      <StripeCards />
+    </>
+  )
+}
+
+export default App
---
+++ src/StripeCards.tsx
@@ -0,0 +1,13 @@
+export function StripeCards() {
+  return (
+    <div className="container" >
+      <div className="card card-0" >
+        <p>some text</p>
+      </div>
+      <div className="card card-1" />
+      <div className="card card-2" />
+      <div className="card card-3" />
+      <div className="card card-4" />
+    </div>
+  )
+}

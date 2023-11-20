1655041f47874df95c270a22834abacf06ac2b39
cheersmas <shradhankr0028@gmail.com>
2023-11-20 22:59:33 +0530

    save progress

--- src/demo/Usage.tsx
+++ src/demo/Usage.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from "react";
+import { useState } from "react";
 import { Banners } from "../components/Banners";
 
 import data from "./data.json";
@@ -23,21 +23,56 @@ function CustomCard({ message, name, company }: CardProps) {
   );
 }
 
+type CardPropsV2 = {
+  name: string;
+  img: string;
+};
+
+function CustomCardV2({ img, name }: CardPropsV2) {
+  return (
+    <article className="demo custom-card-v2">
+      <img src={img} alt={name} />
+    </article>
+  );
+}
+
 export default function Usage() {
-  const components = useMemo(() => {
-    return data.map((item) => (
+  const [currentTab, setCurrentTab] = useState<"testimony" | "pictures">(
+    "testimony"
+  );
+
+  function setTab(tab: "testimony" | "pictures") {
+    setCurrentTab(tab);
+  }
+
+  const components = {
+    testimony: data.map((item) => (
       <CustomCard
         key={item.id}
         message={item.message}
         company={item.company}
         name={item.name}
       />
-    ));
-  }, []);
+    )),
+    pictures: data.map((item) => (
+      <CustomCardV2 img={item.img} name={item.name} key={item.id} />
+    )),
+  };
 
   return (
-    <div className="demo-container">
-      <Banners>{components}</Banners>
+    <div className="options">
+      <section className="buttons-container">
+        <button onClick={() => setTab("testimony")}>Testimonies</button>
+        <button onClick={() => setTab("pictures")}>Pictures</button>
+      </section>
+      <div className="demo-container">
+        {currentTab === "pictures" && (
+          <Banners>{components[currentTab]}</Banners>
+        )}
+        {currentTab === "testimony" && (
+          <Banners>{components[currentTab]}</Banners>
+        )}
+      </div>
     </div>
   );
 }
